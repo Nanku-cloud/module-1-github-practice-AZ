b1b36ee400c4eb871783cc4d7915ce94a5baf704
Nanku-cloud <asahelzuniga06@gmail.com>
2026-08-31 01:41:59 +0000

    Filter Creator Queue by status

--- M2_A2_Creator_Queue_Student_Files/src/screens/CreatorQueueScreen.js
+++ M2_A2_Creator_Queue_Student_Files/src/screens/CreatorQueueScreen.js
@@ -79,7 +79,12 @@ export default function CreatorQueueScreen() {
 
   // Create filteredPosts so All shows everything
   // and each other filter shows only matching statuses.
-  const filteredPosts = posts;
+  const filteredPosts =
+  selectedFilter === 'All'
+    ? posts
+    : posts.filter(
+        (post) => post.status === selectedFilter
+      );
 
   const publishedCount = posts.filter(
     (post) => post.status === 'Published'
