42cc6a0c847e34890435746b2f9aa4dbbc1dcbea
Nanku-cloud <asahelzuniga06@gmail.com>
2026-09-06 19:40:49 +0000

    Add Pulse Feed tab filtering

--- M3_A2_Pulse_Feed_Student_Files/src/screens/FeedScreen.js
+++ M3_A2_Pulse_Feed_Student_Files/src/screens/FeedScreen.js
@@ -25,7 +25,13 @@ export default function FeedScreen() {
 }, []);
 
   // TODO 5: Filter the Following feed in Step 6.
-  const visiblePosts = posts;
+  const visiblePosts = useMemo(() => {
+  if (selectedFeed === 'following') {
+    return posts.filter((post) => post.following);
+  }
+
+  return posts;
+}, [selectedFeed]);
 
   function handleLike(id) {
     // TODO 6: Toggle likedIds in Step 7.
